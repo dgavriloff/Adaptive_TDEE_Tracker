090e6e1f3e905f2b23b8ccd4839847d89f13e10d
dgavriloff <denis.gavriloff@gmail.com>
2024-08-01 14:10:06 -0700

    Cleaned up repo -removed unused folders and files

--- csv-script/test-scripts.js
+++
@@ -1,6 +0,0 @@
-const getDayofWeekFromDateId = (dateId) => {
-  return new Date(dateId.slice(0,4), parseInt(dateId.slice(4,6))-1, parseInt(dateId.slice(6,8)), 0, 0,0,0,);
-}
-
-console.log();
-console.log(new Date(getDayofWeekFromDateId('20240611').getTime() + 345600000));
