981de4b1872cf45da073bf71268896a861b46987
dgavriloff <denis.gavriloff@gmail.com>
2024-09-23 13:11:10 -0700

    Added contextual infomation in BasicInformation  -Due to apples request to remove the need of the personal information such as gender and age,  I added context for why the app needs that information for its core functionality.

--- tdee-tracker/screens/registration-forms/BasicInformation.js
+++ tdee-tracker/screens/registration-forms/BasicInformation.js
@@ -1,15 +1,16 @@
 import React, { useState, useContext } from "react";
-import { View, StyleSheet, ScrollView, Text} from "react-native";
+import { View, StyleSheet, ScrollView, Text, Linking } from "react-native";
 import BubbleButton from "../../components/BubbleButton";
 import { UserDataContext } from "../../components/UserDataProvider";
 import DismissKeyboard from "../../components/DismissKeyboard";
 import LabeledInput from "../../components/LabeledInput";
 import Segment from "../../components/Segment";
-
-import { showMessage, hideMessage } from "react-native-flash-message";
+import { ThemeContext } from "../../components/ThemeProvider";
 
 const BasicInformation = ({ navigation }) => {
-  const { updateUserData, userData, isInputValid } = useContext(UserDataContext);
+  const { updateUserData, userData, isInputValid } =
+    useContext(UserDataContext);
+  const { currentTheme } = useContext(ThemeContext);
   const [age, setAge] = useState(userData.age ? userData.age.toString() : "");
   const [startWeight, setStartWeight] = useState(
     userData.startWeight ? userData.startWeight.toString() : ""
@@ -23,21 +24,24 @@ const BasicInformation = ({ navigation }) => {
   const [missingFields, setMissingFields] = useState(false);
 
   const handleNext = () => {
-    if (isInputValid(age, 10, 150, 'Age') &&  isInputValid(height, 20, 250, 'Height') && isInputValid(startWeight, 20, 1000, 'Start weight')) {
+    if (
+      isInputValid(age, 10, 150, "Age") &&
+      isInputValid(height, 20, 250, "Height") &&
+      isInputValid(startWeight, 20, 1000, "Start weight")
+    ) {
       updateUserData({
         age: parseFloat(age),
         startWeight: parseFloat(startWeight),
         gender: gender,
         height: parseFloat(height),
-        currentWeight: parseFloat(startWeight)
+        currentWeight: parseFloat(startWeight),
       });
       setMissingFields(false);
-      navigation.navigate("Activity Level")
+      navigation.navigate("Activity Level");
     } else {
       setMissingFields(true);
     }
   };
-
 
   const handleBack = () => {
     updateUserData({
@@ -49,6 +53,44 @@ const BasicInformation = ({ navigation }) => {
     setMissingFields(false);
     navigation.goBack();
   };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#f0f0f0",
+    },
+    scrollContainer: {
+      width: "100%",
+      paddingBottom: 25,
+      alignItems: "center",
+    },
+    buttonContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      width: "85%",
+      bottom: 0,
+    },
+    weightContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      width: "100%",
+      justifyContent: "space-between",
+      marginTop: 10,
+      marginBottom: 10,
+    },
+    weightButtons: {
+      flexDirection: "row",
+    },
+    labelText: {
+      fontSize: 16,
+      textAlign: "center",
+    },
+    context: {
+      color: currentTheme.buttonTextColor,
+      fontWeight: "800",
+      marginTop: 10,
+    },
+  });
 
   return (
     <View style={styles.container}>
@@ -64,7 +106,9 @@ const BasicInformation = ({ navigation }) => {
             />
 
             <LabeledInput
-              units={userData.heightUnits === 'in' ? 'inches' : userData.heightUnits}
+              units={
+                userData.heightUnits === "in" ? "inches" : userData.heightUnits
+              }
               placeholder={"Enter height"}
               value={height}
               onChangeText={setHeight}
@@ -95,6 +139,16 @@ const BasicInformation = ({ navigation }) => {
             />
           </View>
         </Segment>
+        <Text
+          style={styles.context}
+          onPress={() =>
+            Linking.openURL(
+              "https://read.qxmd.com/read/2305711/a-new-predictive-equation-for-resting-energy-expenditure-in-healthy-individuals?redirected=slug"
+            )
+          }
+        >
+          Why do we need this information?
+        </Text>
 
         <View style={styles.buttonContainer}>
           <BubbleButton
@@ -112,38 +166,5 @@ const BasicInformation = ({ navigation }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f0f0f0",
-  },
-  scrollContainer: {
-    width: "100%",
-    paddingBottom: 25,
-    alignItems: "center",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "85%",
-    bottom: 0,
-  },
-  weightContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    width: "100%",
-    justifyContent: "space-between",
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  weightButtons: {
-    flexDirection: "row",
-  },
-  labelText: {
-    fontSize: 16,
-    textAlign: "center",
-  },
-});
 
 export default BasicInformation;
